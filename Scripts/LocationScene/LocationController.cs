08e62b4754911ce881fd1a5f5d74ea10622d82f5
Jessica <wuj130@gene.com>
2019-08-29 16:34:47 -0700

    first commit

---
+++ Scripts/LocationScene/LocationController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LocationController : MonoBehaviour
+{
+    public GameObject[] Pokemons;
+    public string[] GPSLocations;
+    public Transform Trainer;
+    public GameObject[] ARLocations;
+    public Vector3[] gamePositions;
+    public Transform Map;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // // Update is called once per frame
+    void Update()
+    {
+        var ARTriggers = GameObject.FindGameObjectsWithTag("randomCharizard");
+        print(ARTriggers);
+        // var ARTriggers = GameObject.FindWithTag("randomCharizard");  //change this to list of possible GameObjects
+
+        foreach (var ARTrigger in ARTriggers)
+        {
+            var distance = Vector3.Distance(ARTrigger.transform.position, Trainer.position);
+            if (distance <= 2f)
+            {
+                SceneManager.LoadScene("TriggeredARScene");
+            }
+        }
+
+    }
+}
