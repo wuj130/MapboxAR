aaff9e5abb98c83c5987a51418ea9c32764a754f
Jessica <wuj130@gene.com>
2019-09-04 13:51:13 -0700

    added commented out code to LocationController

--- Scripts/MapScene/LocationController.cs
+++ Scripts/MapScene/LocationController.cs
@@ -1,42 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Mapbox.Utils;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Mapbox.Unity.Map;
+using Mapbox.Unity.Utilities;
 
 public class LocationController : MonoBehaviour
 {
-    public Transform Trainer;
-<<<<<<< HEAD:Scripts/MapScene/LocationController.cs
-<<<<<<< HEAD:Scripts/MapScene/LocationController.cs
-    public Transform Map;
-=======
->>>>>>> parent of 8b06867... merge fix:Scripts/MapScene/LocationController.cs
-=======
->>>>>>> parent of 8b06867... merge fix:Scripts/MapScene/LocationController.cs
+    public Transform Player;
 
-    // Start is called before the first frame update
+    public AbstractMap Map;
+    // public string[] _locationStrings;
+    // public float _spawnScale = 5f;
+    // public GameObject _markerPrefab;
+
+
+    // // Unsure
+	// Vector2d[] _locations;
+	// List<GameObject> _spawnedObjects;
+
+    
+    // void PlaceARMarkers()
+	// {
+	// 	void Update()
+	// 	{
+	// 		int count = _spawnedObjects.Count;
+	// 		for (int i = 0; i < count; i++)
+	// 		{
+	// 			var spawnedObject = _spawnedObjects[i];
+	// 			var location = _locations[i];
+	// 			spawnedObject.transform.localPosition = _map.GeoToWorldPosition(location, true);
+	// 			spawnedObject.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
+	// 		}
+	// 	}
+    // }
+
     void Start()
     {
-
+        // _locations = new Vector2d[_locationStrings.Length];
+        // _spawnedObjects = new List<GameObject>();
+        // for (int i = 0; i < _locationStrings.Length; i++)
+        // {
+        //     var locationString = _locationStrings[i];
+        //     _locations[i] = Conversions.StringToLatLon(locationString);
+        //     var pos = Map.GeoToWorldPosition(_locations[i], true);
+        //     var instance = Instantiate(_markerPrefab, pos, new Quaternion());
+        //     // instance.transform.localPosition = Map.GeoToWorldPosition(_locations[i], true);
+        //     instance.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
+        //   _spawnedObjects.Add(instance);
+		// } 
     }
 
     // // Update is called once per frame
     void Update()
     {
-<<<<<<< HEAD:Scripts/MapScene/LocationController.cs
-<<<<<<< HEAD:Scripts/MapScene/LocationController.cs
         var ARTriggers = GameObject.FindGameObjectsWithTag("randomCharizard");
-=======
-        var ARTriggers = GameObject.FindGameObjectsWithTag("randomBigCharizard");
->>>>>>> parent of 8b06867... merge fix:Scripts/MapScene/LocationController.cs
-=======
-        var ARTriggers = GameObject.FindGameObjectsWithTag("randomBigCharizard");
->>>>>>> parent of 8b06867... merge fix:Scripts/MapScene/LocationController.cs
-
         foreach (var ARTrigger in ARTriggers)
         {
-            var distance = Vector3.Distance(ARTrigger.transform.position, Trainer.position);
-            if (distance <= 2f)
+            var distance = Vector3.Distance(ARTrigger.transform.position, Player.position);
+            if (distance <= 3f)
             {
                 SceneManager.LoadScene("TriggeredARScene");
             }
