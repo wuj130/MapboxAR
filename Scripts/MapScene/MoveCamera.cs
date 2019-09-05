8d19cb005569a8b3ac50ead821c3494231b39429
Jessica <wuj130@gene.com>
2019-09-05 14:48:33 -0700

    fixed pan/zoom, working on recentering

---
+++ Scripts/MapScene/MoveCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MoveCamera : MonoBehaviour
+{
+    public Camera cam;
+    public Transform Player;
+    public void Recenter()
+    {
+        cam.ScreenToWorldPoint(Player.localPosition);
+        // SceneManager.LoadScene("Location-basedGame");
+    }
+    
+}
---
+++ Scripts/MapScene/RecenterController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class RecenterController : MonoBehaviour
+{
+    public Camera cam;
+    public Transform Player;
+    public void Recenter()
+    {
+        cam.transform.position = Player.localPosition;
+        // SceneManager.LoadScene("Location-basedGame");
+    }
+    
+}
